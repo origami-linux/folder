b875f9327190966b3b1f7e46f551599be6ca4cd4
Gabry <gabry@voidlinux.home-life.hub>
2021-04-26 15:57:56 +0200

    Finished the tar.xz downloader from the repo

--- source/main.c
+++ source/main.c
@@ -24,8 +24,10 @@ int main(int argc, char *argv[])
 
 	enum mode { INSTALL_PKG } mode;
 
-	while ((opt = getopt(argc, argv, "i:p: yh")) != -1) {
-		switch (opt) {
+	while ((opt = getopt(argc, argv, "i:p: yh")) != -1)
+	{
+		switch (opt)
+		{
 
 		case 'i':
 			mode = INSTALL_PKG;
@@ -42,42 +44,52 @@ int main(int argc, char *argv[])
 	}
 
 	// TODO: Debug this, gives SEGV
-	if (mode == INSTALL_PKG && pkg_name != NULL) {
+	if (mode == INSTALL_PKG && pkg_name != NULL)
+	{
 		CURL *curl;
-		FILE *fp;
-		CURLcode res;
+        FILE *fp;
+        CURLcode res;
 
-		char url[46 + strlen(pkg_name) + 7];
-		strcpy(url, "https://github.com/origami-linux/packages-repo/");
-		strcat(url, pkg_name);
-		strcat(url, ".tar.xz");
-		char outfile[4 + strlen(pkg_name) + 7];
-		strcat(outfile, "/tmp/");
-		strcat(outfile, pkg_name);
-		strcat(outfile, ".tar.xz");
-		curl = curl_easy_init();
-		if (curl)
-		{
-			fp = fopen(outfile,"wb");
-			curl_easy_setopt(curl, CURLOPT_URL, url);
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-			curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
-			fclose(fp);
-			curl_easy_cleanup(curl);
-			if(res != CURLE_OK)
-			{
-				if(res == 440) printf("Package '%s' not found in repo\n", pkg_name);
-				else printf("Curl had error %d\n", res);
+        char url[56 + strlen(pkg_name) + 8];
+        strcpy(url, "https://github.com/origami-linux/packages-repo/raw/main/");
+        strcat(url, pkg_name);
+        strcat(url, ".tar.xz");
+        char outfile[5 + strlen(pkg_name) + 8];
+        strcpy(outfile, "/tmp/");
+        strcat(outfile, pkg_name);
+        strcat(outfile, ".tar.xz");
 
-				// TODO: remove file in temp
-				return -1;
-			}
-		}
-		else
-		{
-			printf("Curl had an error while being initializing\n");
-			return -1;
-		}
-	}
+        curl = curl_easy_init();
+
+        if (curl)
+        {
+            fp = fopen(outfile,"wb");
+            curl_easy_setopt(curl, CURLOPT_URL, url);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
+            curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
+
+			res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            fclose(fp);
+            if(res != CURLE_OK)
+            {
+                if(res >= 400)
+                    printf("Package '%s' not found in repo, error %d\n", pkg_name, res);
+                else
+                    printf("Curl had error %d: '%s'\n", res, curl_easy_strerror(res));
+
+                while(remove(outfile) != 0) {;}
+                return -1;
+            }
+
+			while(remove(outfile) != 0) {;}
+        }
+        else
+        {
+            printf("Curl had an error while being initializing\n");
+            return -1;
+        }
+    }
 }
